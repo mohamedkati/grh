923992daea87a586949729f9f61b3cd694c59498
mohamedkati <mohamed.katti.7@gmail.com>
2025-08-13 19:48:17 +0100

    Test et apprendre git

--- employee-list.php
+++ employee-list.php
@@ -27,21 +27,23 @@ include 'includes/sidebar.php';
                                 <tbody>
                                     <tr>
                                         <td>1</td>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
+                                        <td>Khawla</td>
+                                        <td>Fadili</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Mohammed</td>
+                                        <td>KATI</td>
                                         <td>@mdo</td>
                                     </tr>
                                     <tr>
                                         <td>2</td>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
+                                        <td>Ouissal</td>
+                                        <td>KATI</td>
                                         <td>@fat</td>
                                     </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td colspan="2">Larry the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
+
                                 </tbody>
                             </table>
                         </div>
